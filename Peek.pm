e4f016cde95dc2758a3c74404cd584405754d51c
H.Merijn Brand <h.m.brand@xs4all.nl>
2010-03-16 11:45:36 +0000

    Import of HMBRAND/Data-Peek-0.31 from CPAN.
    gitpan-cpan-distribution: Data-Peek
    gitpan-cpan-version:      0.31
    gitpan-cpan-path:         HMBRAND/Data-Peek-0.31.tgz
    gitpan-cpan-author:       HMBRAND
    gitpan-cpan-maturity:     released

--- Peek.pm
+++ Peek.pm
@@ -6,7 +6,7 @@ use warnings;
 use DynaLoader ();
 
 use vars qw( $VERSION @ISA @EXPORT @EXPORT_OK );
-$VERSION   = "0.30";
+$VERSION   = "0.31";
 @ISA       = qw( DynaLoader Exporter );
 @EXPORT    = qw( DDumper DDsort DPeek DDisplay DDump DDual DGrow );
 @EXPORT_OK = qw( triplevar );
